c41f83e0aa99af924b6871271e9e3fd492a93dcd
tomoaoihana <tamu.kusanishi@gmail.com>
2025-01-09 16:50:42 +0900

    ✨ astro.config.mjsにサイトURLを追加し、robots.txtを新規作成。Layout.astroにsitemapリンクを追加。

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [sitemap()]
+  site: "https://shibajuku.net/",
+  integrations: [sitemap()],
 });
